aba74795b821be801bf747ef17814e7f9a3295ce
Anton Pashinov <troxin1988@gmail.com>
2024-03-28 16:10:24 +0700

    Add docstring to read_json

--- operations/utils.py
+++ operations/utils.py
@@ -2,6 +2,11 @@ import json
 
 
 def read_json(filename: str) -> list | None:
+    """
+    Функция читает файл json.
+    :param filename: Путь к файлу
+    :return: Список словарей, если файл найден, иначе None
+    """
     try:
         with open(filename, "r", encoding="UTF-8") as file:
             all_operations = json.load(file)
